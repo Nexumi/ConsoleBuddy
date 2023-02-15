08c634075abfe95c734b490068a7aa637d5de535
Nexumi <48866805+Nexumi@users.noreply.github.com>
2023-02-15 14:20:31 -0800

    Add files via upload

--- consoleBuddy.py
+++ consoleBuddy.py
@@ -7,13 +7,13 @@ def find(directory, folder, program):
     if os.path.exists(directory):
         for file in os.listdir(directory):
             if file.lower() == folder.lower():
-                if program in os.listdir(directory + "/" + file):
-                    return directory + "/" + file + "/" + program
+                if program in os.listdir(directory + "\\" + file):
+                    return directory + "\\" + file + "\\" + program
 
 def locate(folder, program):
     drives = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     for letter in drives:
-        directory = letter + ":/Program Files"
+        directory = letter + ":\\Program Files"
         found = find(directory, folder, program)
         if found:
             return found
@@ -27,19 +27,37 @@ def header(line = ""):
     print(("\033[4mCurrent Directory: " + os.getcwd().split("\\")[-1] + "\033[0m").center(os.get_terminal_size().columns))
     print("    ".join(os.listdir()) + line)
 
-def fuzzy(path):
+def display(name):
+    for letter in name:
+        char = ord(letter)
+        if char >= 65 and char <= 90:
+            print(" " + letter, end = "")
+        else:
+            print(letter, end = "")
+    print()
+
+def grader(done):
+    os.system("cls")
+    print(("\033[4m" + os.getcwd() + "\033[0m"))
+    for idir in os.listdir():
+        xdir = idir.split("_")
+        if xdir not in done and len(xdir) == 4:
+            name = xdir[3].split("-")[0]
+            display(name)
+
+def fuzzy(file, path = "."):
     dirs = []
-    listdir = os.listdir()
+    listdir = os.listdir(path)
     for idir in listdir:
-        if idir.lower() == path.lower():
+        if idir.lower() == file.lower():
             return idir
 
-        if idir.lower().find(path.lower()) != -1:
+        if idir.lower().find(file.lower()) != -1:
             dirs.append(idir)
 
     ld = len(dirs)
     if ld == 0:
-        return path
+        return file
     elif ld == 1:
         return dirs[0]
     else:
@@ -52,7 +70,7 @@ def fuzzy(path):
             return dirs[opt]
         except:
             header("\n")
-            return path
+            return file
 
 def getRubric():
     listdir = os.listdir()
@@ -65,6 +83,7 @@ def getRubric():
             return results
 
 def command(cmd):
+    global rubrics
     try:
         cmd = cmd.split(" ", 1)
         cmd[0] = cmd[0].lower()
@@ -134,11 +153,13 @@ def command(cmd):
                 subprocess.Popen([sublime_text, fuzzy(" ".join(program[2:]))])
         elif cmd[0] == "eval":
             eval(cmd[1])
-        elif cmd[0] == "programs":
+        elif cmd[0] == "locations":
             if notepad_plus_plus:
                 print(notepad_plus_plus)
             if sublime_text:
                 print(sublime_text)
+            if rubrics:
+                print(rubrics)
         elif cmd[0] == "generate":
             if len(cmd) == 1:
                 result = getRubric()
@@ -146,6 +167,27 @@ def command(cmd):
                 return
             files = cmd[1].split()
             os.system("generateStudentRubrics.exe --assignment=\"" + files[1][11:files[1].find("-Rubric.xlsx")] + "\" --inputFile=" + files[0] + " --fileToCopy=" + files[1])
+        elif cmd[0] == "set":
+            if cmd[1].lower() == "rubrics":
+                rubrics = os.getcwd()
+                print("rubics = " + rubrics)
+        elif cmd[0] == "rubric":
+            if len(cmd) != 1 and rubrics:
+                os.startfile(rubrics + "\\" + fuzzy(cmd[1], rubrics))
+                header()
+        elif cmd[0] == "pretty":
+            os.system("cls")
+            print(("\033[4mCurrent Directory: " + os.getcwd().split("\\")[-1] + "\033[0m").center(os.get_terminal_size().columns))
+            for idir in os.listdir():
+                xdir = idir.split("_")
+                if len(xdir) == 4:
+                    name = xdir[3].split("-")[0]
+                    display(name)
+                elif len(xdir) == 5:
+                    name = xdir[4].split("-")[0]
+                    display(name)
+                else:
+                    print(" * " + idir)
         else:
             os.system(" ".join(cmd))
     except Exception as e:
@@ -154,10 +196,11 @@ def command(cmd):
 cmd = ""
 notepad_plus_plus = locate("Notepad++", "notepad++.exe")
 sublime_text = locate("Sublime Text", "sublime_text.exe")
+rubrics = None
 
 header("\n")
-while cmd.lower() != "exit":
-    if cmd != "":
+while cmd.lower().strip() != "exit":
+    if cmd.strip() != "":
         print()
     cmd = " ".join(input("Console> ").split())
     header("\n")
