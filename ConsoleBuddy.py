c20c0f628b1b677e58792f9219a68e5021e40e7c
Nexumi <48866805+Nexumi@users.noreply.github.com>
2023-03-20 18:56:54 -0700

    Add files via upload

--- ConsoleBuddy.py
+++ ConsoleBuddy.py
@@ -1,11 +1,18 @@
-v = "v0.3.2"
+v = "v0.4"
 
 import os
+import ssl
 from zipfile import ZipFile
 from subprocess import Popen
 from shutil import copy2, rmtree
 from webbrowser import open as web
 from urllib.request import urlopen, urlretrieve
+
+def clear():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
 
 def reload():
     global cmd
@@ -20,8 +27,8 @@ def update():
         r = str(line)[7:-6]
         data.close()
     if v != r:
-        print("[\033[34mnotice\033[0m] A new release of ConsoleBuddy is available: \033[31m" + v + "\033[0m -> \033[32m" + r + "\033[0m")
-        print("[\033[34mnotice\033[0m] To go to download page, run: \033[32mdownload\033[0m")
+        output.append("[\033[34mnotice\033[0m] A new release of ConsoleBuddy is available: \033[31m" + v + "\033[0m -> \033[32m" + r + "\033[0m")
+        output.append("[\033[34mnotice\033[0m] To go to download page, run: \033[32mdownload\033[0m")
         cmd = "update"
 
 def find(directory, folder, program):
@@ -43,19 +50,33 @@ def locate(folder, program):
         if found:
             return found
 
-def header(line = ""):
-    os.system("cls")
+def header():
+    global skip
+    if skip:
+        skip = False
+        return
+    global output
+    clear()
     print(("\033[4mCurrent Directory: " + os.getcwd().split("\\")[-1] + "\033[0m").center(os.get_terminal_size().columns))
-    print("    ".join(os.listdir()) + line)
+    print("  |  ".join(os.listdir()) + "\n")
+    if cmd.strip() != "" and output:
+        for i in range(len(output)):
+            if type(output[i]) == type(b""):
+                output[i] = output[i].decode("utf-8")
+            else:
+                output[i] = str(output[i])
+        print("\n".join(output) + "\n")
+    output.clear()
 
-def display(name):
+def build(name):
+    n = ""
     for letter in name:
         char = ord(letter)
         if char >= 65 and char <= 90:
-            print(" " + letter, end = "")
+            n += " " + letter
         else:
-            print(letter, end = "")
-    print()
+            n += letter
+    output.append(n)
 
 def fuzzy(file, path = "."):
     dirs = []
@@ -72,14 +93,12 @@ def fuzzy(file, path = "."):
         return dirs[0]
     else:
         for i in range(ld):
-            print(str(i + 1) + ") " + dirs[i])
-        print()
+            output.append(str(i + 1) + ") " + dirs[i])
+        header()
         try:
             opt = int(input("Option> ")) - 1
-            header("\n")
             return dirs[opt]
         except:
-            header("\n")
             return file
 
 def unzipper():
@@ -125,11 +144,11 @@ def generate(cmd, refresh = True):
         missing = "rubric"
         if not names:
             missing += " and grader"
-        print("404 " + missing + " not found")
+        output = "404 " + missing + " not found"
         return False
     data.close()
     if not names:
-        print("404 grader not found")
+        output = "404 grader not found"
         return False
     os.mkdir(folder)
     os.chdir(folder)
@@ -143,7 +162,16 @@ def generate(cmd, refresh = True):
         header()
     return True
 
+def native(cmd):
+    global skip
+    header()
+    os.system(cmd)
+    if cmd.strip() != "":
+        print()
+    skip = True
+
 def command(cmd):
+    global output
     global rubrics
     global top
     try:
@@ -151,130 +179,119 @@ def command(cmd):
         cmd[0] = cmd[0].lower()
         if cmd[0] == "cd":
             if len(cmd) == 1:
-                os.system(cmd[0])
+                native(cmd[0])
                 return
             os.chdir(fuzzy(cmd[1]))
-            header()
         elif cmd[0] == "del":
             if len(cmd) == 1:
-                os.system(cmd[0])
+                native(cmd[0])
                 return
             path = fuzzy(cmd[1])
             if path == ".":
                 header("\n")
-                print("WARNING: del . is disabled as it deletes everything in the current directory.")
-                print("If you want to delete everything, delete the folder: cd .. > del folder_name")
+                output.append("WARNING: del . is disabled as it deletes everything in the current directory.")
+                output.append("If you want to delete everything, delete the folder: cd .. > del folder_name")
                 return
             if os.path.isfile(path):
                 os.remove(path)
             else:
                 rmtree(path)
-            header()
         elif cmd[0] == "start":
             if len(cmd) == 1:
-                os.system(cmd[0])
+                native(cmd[0])
                 return
             os.startfile(fuzzy(cmd[1]))
-            header()
         elif cmd[0] == "copy":
             if len(cmd) == 1:
-                os.system(cmd[0])
+                native(cmd[0])
                 return
             parm = cmd[1].split(" ")
             copy2(fuzzy(parm[0]), fuzzy(parm[1]))
-            header()
         elif cmd[0] == "move":
             if len(cmd) == 1:
-                os.system(cmd[0])
+                native(cmd[0])
                 return
             parm = cmd[1].split(" ")
-            os.system(cmd[0] + " \"" + fuzzy(parm[0]) + "\" \"" + fuzzy(parm[1] + "\""))
+            native(cmd[0] + " \"" + fuzzy(parm[0]) + "\" \"" + fuzzy(parm[1] + "\""))
         elif cmd[0] == "java":
             if len(cmd) == 1:
-                os.system(cmd[0])
+                native(cmd[0])
                 return
-            os.system("java " + fuzzy(cmd[1]).replace(".class", ""))
+            native("java " + fuzzy(cmd[1]).replace(".class", ""))
         elif cmd[0] == "unzipper":
             unzipper()
-            header()
         elif cmd[0] == "startwith":
             program = cmd[1].split()
-            if notepad_plus_plus and program[0].lower() == "notepad++":
+            if programs.get("Notepad++") and program[0].lower() == "notepad++":
                 java = fuzzy(" ".join(program[1:]))
-                Popen([notepad_plus_plus, java])
-                print("Opening " + java)
-            elif sublime_text and " ".join(program[:2]).lower() == "sublime text":
+                Popen([programs.get("Notepad++"), java])
+                output.append("Opening " + java)
+            elif programs.get("Sublime Text") and " ".join(program[:2]).lower() == "sublime text":
                 java = fuzzy(" ".join(program[2:]))
-                Popen([sublime_text, java])
-                print("Opening " + java)
-            elif sublime_text and program[0].lower() == "sublime":
+                Popen([programs.get("Sublime Text"), java])
+                output.append("Opening " + java)
+            elif programs.get("Sublime Text") and program[0].lower() == "sublime":
                 java = fuzzy(" ".join(program[1:]))
-                Popen([sublime_text, java])
-                print("Opening " + java)
+                Popen([programs.get("Sublime Text"), java])
+                output.append("Opening " + java)
             else:
-                print("Program not found or not defined")
+                output.append("Program not found or unsupported")
         elif cmd[0] == "eval":
             eval(cmd[1])
         elif cmd[0] == "programs":
-            if notepad_plus_plus:
-                print(notepad_plus_plus)
-            if sublime_text:
-                print(sublime_text)
+            for program in programs.values():
+                output.append(program)
         elif cmd[0] == "generate":
             if len(cmd) == 1:
-                print("The syntax of the command is incorrect.")
+                output.append("The syntax of the command is incorrect.")
                 return
             cmd[1] = cmd[1].split(" ", 1)
             if len(cmd[1]) == 1:
-                print("The syntax of the command is incorrect.")
+                output.append("The syntax of the command is incorrect.")
                 return
             generate(cmd[1])
         elif cmd[0] == "set":
             if cmd[1].lower() == "rubrics":
                 rubrics = os.getcwd()
-                print("rubrics = " + rubrics)
+                output.append("rubrics = " + rubrics)
             elif cmd[1].lower() == "top":
                 top = os.getcwd()
-                print("top = " + top)
+                output.append("top = " + top)
         elif cmd[0] == "rubric":
             if not rubrics:
-                print("Location not set")
+                output.append("Location not set")
             else:
                 if len(cmd) == 1:
-                    print("rubrics = " + rubrics)
+                    output.append("rubrics = " + rubrics)
                 else:
                     rubric = fuzzy(cmd[1], rubrics)
                     os.startfile(rubrics + "\\" + rubric)
-                    print("Opening " + rubric)
+                    output.append("Opening " + rubric)
         elif cmd[0] == "pretty":
-            os.system("cls")
-            print(("\033[4mCurrent Directory: " + os.getcwd().split("\\")[-1] + "\033[0m").center(os.get_terminal_size().columns))
             for idir in os.listdir():
                 xdir = idir.split("_")
                 if len(xdir) == 4:
                     name = xdir[3].split("-")[0]
-                    display(name)
+                    build(name)
                 elif len(xdir) == 5:
                     name = xdir[4].split("-")[0]
-                    display(name)
+                    build(name)
                 else:
-                    print(" * " + idir)
+                    output.append(" * " + idir)
         elif cmd[0] == "top":
             os.chdir(top)
-            header()
         elif cmd[0] == "download":
             web("https://github.com/Nexumi/ConsoleBuddy/releases")
-            header()
         elif cmd[0] == "version":
-            print("ConsoleBuddy " + v)
+            output.append("ConsoleBuddy " + v)
             update()
         elif cmd[0] == "setup":
             if len(cmd) == 1:
-                print("The syntax of the command is incorrect.")
+                output.append("The syntax of the command is incorrect.")
                 return
             cmd[1] = cmd[1].split(" ", 1)
             if len(cmd[1]) == 1:
-                print("The syntax of the command is incorrect.")
+                output.append("The syntax of the command is incorrect.")
                 return
             if "submissions.zip" in os.listdir():
                 assignment = "Assignment-" + cmd[0]
@@ -285,36 +302,50 @@ def command(cmd):
                 unzipper()
                 generate(cmd[1], False)
                 os.chdir("..")
-                header()
             else:
-                print("submissions.zip not found")
+                output.append("submissions.zip not found")
         elif cmd[0] == "assignment":
             if len(cmd) == 1:
-                print("The syntax of the command is incorrect.")
+                output.append("The syntax of the command is incorrect.")
                 return
             web("https://csc210.ducta.net/Assignments/Assignment-" + cmd[1] + ".pdf")
-            header()
         else:
-            os.system(" ".join(cmd))
+            native(" ".join(cmd))
         return True
     except Exception as e:
-        print(e)
+        output.append(e)
         return False
 
+if os.name != "nt":
+    ssl._create_default_https_context = ssl._create_unverified_context
+    try:
+        chdir(path.sep.join(argv[0].split(path.sep)[:-1]))
+    except:
+        pass
+
 # TODO: List programs into a Dictionary datatype
-notepad_plus_plus = locate("Notepad++", "notepad++.exe")
-sublime_text = locate("Sublime Text", "sublime_text.exe")
+if os.name == "nt":
+    programs = {}
+    programs["Notepad++"] = locate("Notepad++", "notepad++.exe")
+    programs["Sublime Text"] = locate("Sublime Text", "sublime_text.exe")
+    poppers = []
+    for program in programs.keys():
+        if not programs.get(program):
+            poppers.append(program)
+    for popper in poppers:
+        programs.pop(popper)
+    del poppers
 rubrics = None
 top = os.getcwd()
 
-header("\n")
 cmd = ""
+skip = False
+output = []
 update()
+header()
 while cmd.lower().strip() != "exit":
-    if cmd.strip() != "":
-        print()
     cmd = " ".join(input("Console> ").split())
-    header("\n")
     command(cmd)
+    header()
 
-os.system("cls")
+clear()
