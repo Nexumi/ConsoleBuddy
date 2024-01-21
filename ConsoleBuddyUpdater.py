598079b53e23f047062bf9292a7d5139f6b382c7
Nexumi <48866805+Nexumi@users.noreply.github.com>
2024-01-21 00:32:18 -0800

    Add files via upload

--- ConsoleBuddyUpdater.py
+++ ConsoleBuddyUpdater.py
@@ -2,8 +2,22 @@ import os
 from time import sleep
 from urllib.request import urlretrieve
 
+def launch(filename):
+    if os.name == "nt":
+        os.startfile(filename)
+    else:
+        os.system("x-terminal-emulator -e ./" + filename)
+
 sleep(1)
-if "ConsoleBuddy.exe" in os.listdir():
-	os.remove("ConsoleBuddy.exe")
-urlretrieve("https://github.com/Nexumi/ConsoleBuddy/releases/latest/download/ConsoleBuddy.exe", "ConsoleBuddy.exe")
-os.startfile("ConsoleBuddy.exe")
+dirs = os.listdir()
+if os.name == "nt":
+	if "ConsoleBuddy.exe" in dirs:
+		os.remove("ConsoleBuddy.exe")
+	urlretrieve("https://github.com/Nexumi/ConsoleBuddy/releases/latest/download/ConsoleBuddy.exe", "ConsoleBuddy.exe")
+	launch("ConsoleBuddy.exe")
+else:
+	if "ConsoleBuddy" in dirs:
+		os.remove("ConsoleBuddy")
+	urlretrieve("https://github.com/Nexumi/ConsoleBuddy/releases/latest/download/ConsoleBuddy", "ConsoleBuddy")
+	os.system("chmod +x ConsoleBuddy")
+	launch("ConsoleBuddy")
